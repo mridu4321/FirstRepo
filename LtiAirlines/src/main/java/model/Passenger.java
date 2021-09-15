a9395a876cdec8160903398ed83e1f629757a914
mridu4321 <mridula4321@gmail.com>
2021-09-15 13:14:25 +0530

    Added passenger from br1

---
+++ LtiAirlines/src/main/java/model/Passenger.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Passenger {
+	int passengerId;
+	String passengerName;
+	public int getPassengerId() {
+		return passengerId;
+	}
+	public void setPassengerId(int passengerId) {
+		this.passengerId = passengerId;
+	}
+	public String getPassengerName() {
+		return passengerName;
+	}
+	public void setPassengerName(String passengerName) {
+		this.passengerName = passengerName;
+	}
+	
+	
+	
+
+}
